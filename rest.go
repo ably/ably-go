2e56fdcce66b7fb5d86bfd6942a8989588e30223
Lewis Marshall <lewis@lmars.net>
2014-08-21 13:20:33 +0100

    Support GET /time

---
+++ rest.go
@@ -0,0 +1,96 @@
+package ably
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type Params struct {
+	Endpoint  string
+	AppID     string
+	AppSecret string
+}
+
+func RestClient(params Params) *Client {
+	return &Client{Params: params}
+}
+
+type Client struct {
+	Params
+}
+
+func (c *Client) Time() (*time.Time, error) {
+	times := []int64{}
+	_, err := c.Get("/time", &times)
+	if err != nil {
+		return nil, err
+	}
+	if len(times) != 1 {
+		return nil, fmt.Errorf("Expected 1 timestamp, got %d", len(times))
+	}
+	t := time.Unix(times[0]/1000, times[0]%1000)
+	return &t, nil
+}
+
+func (c *Client) Get(path string, data interface{}) (*http.Response, error) {
+	req, err := http.NewRequest("GET", c.Endpoint+path, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Accept", "application/json")
+	req.SetBasicAuth(c.AppID, c.AppSecret)
+	res, err := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	if !c.ok(res.StatusCode) {
+		return res, fmt.Errorf("Unexpected status code %d", res.StatusCode)
+	}
+	return res, json.NewDecoder(res.Body).Decode(data)
+}
+
+func (c *Client) Post(path string, in, out interface{}) (*http.Response, error) {
+	buf, err := json.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("POST", c.Endpoint+path, bytes.NewBuffer(buf))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+	req.SetBasicAuth(c.AppID, c.AppSecret)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if out != nil && c.ok(res.StatusCode) {
+		defer res.Body.Close()
+		return res, json.NewDecoder(res.Body).Decode(out)
+	}
+	if !c.ok(res.StatusCode) {
+		return res, fmt.Errorf("Unexpected status code %d", res.StatusCode)
+	}
+	return res, nil
+}
+
+func (c *Client) Delete(path string) (*http.Response, error) {
+	req, err := http.NewRequest("DELETE", c.Endpoint+path, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.SetBasicAuth(c.AppID, c.AppSecret)
+	res, err := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	if !c.ok(res.StatusCode) {
+		return res, fmt.Errorf("Unexpected status code %d", res.StatusCode)
+	}
+	return res, nil
+}
+
+func (c *Client) ok(status int) bool {
+	return status == http.StatusOK || status == http.StatusCreated
+}
---
+++ rest_test.go
@@ -0,0 +1,78 @@
+package ably
+
+import (
+	"fmt"
+	"log"
+	"testing"
+
+	. "gopkg.in/check.v1"
+)
+
+type testAppNamespace struct {
+	ID        string `json:"id"`
+	Persisted bool   `json:"persisted"`
+}
+
+type testAppKey struct {
+	ID    string `json:"id,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+type testApp struct {
+	ID         string             `json:"id,omitempty"`
+	Keys       []testAppKey       `json:"keys"`
+	Namespaces []testAppNamespace `json:"namespaces"`
+}
+
+func (t *testApp) AppID() string {
+	return fmt.Sprintf("%s.%s", t.ID, t.Keys[0].ID)
+}
+
+func (t *testApp) AppSecret() string {
+	return t.Keys[0].Value
+}
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type TestSuite struct {
+	testApp *testApp
+	client  *Client
+}
+
+var _ = Suite(&TestSuite{
+	testApp: &testApp{
+		Keys: []testAppKey{{}},
+		Namespaces: []testAppNamespace{
+			{ID: "persisted", Persisted: true},
+		},
+	},
+	client: RestClient(Params{Endpoint: "https://sandbox-rest.ably.io"}),
+})
+
+func (s *TestSuite) SetUpSuite(c *C) {
+	log.Println("Creating test app via", s.client.Endpoint)
+	_, err := s.client.Post("/apps", s.testApp, s.testApp)
+	if err != nil {
+		c.Fatal(err)
+	}
+	log.Println("Got test app with ID", s.testApp.ID)
+	s.client.AppID = s.testApp.AppID()
+	s.client.AppSecret = s.testApp.AppSecret()
+}
+
+func (s *TestSuite) TearDownSuite(c *C) {
+	if s.testApp.ID == "" {
+		return
+	}
+	log.Println("Deleting test app with ID", s.testApp.ID)
+	s.client.Delete("/apps/" + s.testApp.ID)
+}
+
+func (s *TestSuite) TestRestTime(c *C) {
+	time, err := s.client.Time()
+	if err != nil {
+		c.Fatal(err)
+	}
+	c.Assert(time, NotNil)
+}
