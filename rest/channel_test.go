ceab46cf0280122020aa86f298dac645e0402f5b
Vincent Bonmalais <vb.kouno@gmail.com>
2015-02-05 18:28:57 +0000

    Refactor tests to have channel tests in a mirroring file
    Just next to channel.go.

--- rest/channel_test.go
+++ rest/channel_test.go
@@ -2,31 +2,15 @@ package rest_test
 
 import (
 	"github.com/ably/ably-go/rest"
-	. "github.com/ably/ably-go/test/support"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	_ "crypto/sha512"
 )
 
 var (
 	client  *rest.Client
 	channel *rest.Channel
 )
-
-var _ = BeforeSuite(func() {
-	_, err := TestAppInstance.Create()
-	Expect(err).NotTo(HaveOccurred())
-
-	client = rest.NewClient(TestAppInstance.Params)
-	channel = client.Channel("test")
-})
-
-var _ = AfterSuite(func() {
-	_, err := TestAppInstance.Delete()
-	Expect(err).NotTo(HaveOccurred())
-})
 
 var _ = Describe("Channel", func() {
 	var (
--- rest/rest_suite_test.go
+++ rest/rest_suite_test.go
@@ -1,6 +1,11 @@
 package rest_test
 
 import (
+	_ "crypto/sha512"
+
+	"github.com/ably/ably-go/rest"
+	. "github.com/ably/ably-go/test/support"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -11,3 +16,16 @@ func TestRest(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Rest Suite")
 }
+
+var _ = BeforeSuite(func() {
+	_, err := TestAppInstance.Create()
+	Expect(err).NotTo(HaveOccurred())
+
+	client = rest.NewClient(TestAppInstance.Params)
+	channel = client.Channel("test")
+})
+
+var _ = AfterSuite(func() {
+	_, err := TestAppInstance.Delete()
+	Expect(err).NotTo(HaveOccurred())
+})
