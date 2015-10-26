f4605f7f4bf176a7f9124bb872357c1024dc8bc4
Rafal Jeczalik <rjeczalik@gmail.com>
2015-10-26 13:43:00 +0100

    error: fix reading ably.Error from HTTP payload

--- ably/proto/error.go
+++ ably/proto/error.go
@@ -2,12 +2,15 @@ package proto
 
 import "fmt"
 
+// Error describes an error returned via ProtocolMessage.
 type Error struct {
-	StatusCode int    `json:"statusCode" msgpack:"statusCode"`
-	Code       int    `json:"code" msgpack:"code"`
-	Message    string `json:"message" msgpack:"message"`
+	StatusCode int    `json:"statusCode,omitempty" msgpack:"statusCode,omitempty"`
+	Code       int    `json:"code,omitempty" msgpack:"code,omitempty"`
+	Message    string `json:"message,omitempty" msgpack:"message,omitempty"`
+	Server     string `json:"serverId,omitempty" msgpack:"serverId,omitempty"`
 }
 
+// Error implements the builtin error interface.
 func (e *Error) Error() string {
-	return fmt.Sprintf("%d: %d %s", e.StatusCode, e.Code, e.Message)
+	return fmt.Sprintf("%s (status=%d, internal=%d)", e.Message, e.StatusCode, e.Code)
 }
