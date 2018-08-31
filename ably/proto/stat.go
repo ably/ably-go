642e7618a9f845db6a6d4a54917f87c995ab9921
gernest <geofreyernest@live.com>
2018-08-31 16:19:04 +0300

    update Stats to 1.1 spec
    This implements
    
    - TS1
    - TS2
    - TS3
    - TS4
    - TS5
    - TS6
    - TS7
    - TS8
    - TS9

--- ably/proto/stat.go
+++ ably/proto/stat.go
@@ -2,52 +2,18 @@ package proto
 
 import "time"
 
-type Stat struct {
-	// The following fields hold statistical information only for Channels
-	// and Connections fields, for the rest they are going to be always zero.
-	Peak   float64 `json:"peak" codec:"peak"`
-	Mean   float64 `json:"mean" codec:"mean"`
-	Opened float64 `json:"opened" codec:"opened"`
-
-	// The following fields hold statistical information only for
-	// APIRequests and TokenRequests fields, for the rest they are going
-	// to always be zero.
-	Succeeded int64 `json:"succeeded" codec:"succeeded"`
-	Failed    int64 `json:"failed" codec:"failed"`
-
-	// The following fields hold accordinly statistical information for
-	// fields other than Channels and Connections - for them they are
-	// going to always be zero.
-	Count int64  `json:"count" codec:"count"`
-	Data  uint64 `json:"data" codec:"data"`
-}
-
-type StatSummary struct {
-	All      Stat `json:"all" codec:"all"`
-	Messages Stat `json:"messages" codec:"messages"`
-
-	// The following field holds statistical information only for
-	// Persisted field, for the rest it's going to be always zero.
-	Presence Stat `json:"presence" codec:"presence"`
-}
-
-type StatConnSummary struct {
-	All      StatSummary `json:"all" codec:"all"`
-	Realtime StatSummary `json:"realtime" codec:"realtime"`
-}
-
 type Stats struct {
 	Count         int64           `json:"count" codec:"count"`
 	Unit          string          `json:"unit" codec:"unit"`
 	IntervalID    string          `json:"intervalId" codec:"intervalId"`
-	Channels      Stat            `json:"channels" codec:"channels"`
-	APIRequests   Stat            `json:"apiRequests" codec:"apiRequests"`
-	TokenRequests Stat            `json:"tokenRequests" codec:"tokenRequests"`
-	All           StatSummary     `json:"all" codec:"all"`
-	Connections   StatSummary     `json:"connections" codec:"connections"`
-	Persisted     StatSummary     `json:"persisted" codec:"persisted"`
-	Inbound       StatConnSummary `json:"inbound" codec:"inbound"`
-	Outbound      StatConnSummary `json:"outbound" codec:"outbound"`
+	All           MessageTypes    `json:"all" codec:"all"`
+	APIRequests   RequestCount    `json:"apiRequests" codec:"apiRequests"`
+	Channels      ResourceCount   `json:"channels" codec:"channels"`
+	Connections   ConnectionTypes `json:"connections" codec:"connections"`
+	Inbound       MessageTraffick `json:"inbound" codec:"inbound"`
+	Outbound      MessageTraffick `json:"outbound" codec:"outbound"`
+	Persisted     MessageTypes    `json:"persisted" codec:"persisted"`
+	TokenRequests RequestCount    `json:"tokenRequests" codec:"tokenRequests"`
 }
 
 const (
@@ -68,4 +34,42 @@ var (
 
 func IntervalFormatFor(t time.Time, granulatity string) string {
 	return t.Format(intervalFormats[granulatity])
+}
+
+type ResourceCount struct {
+	Peak    float64 `json:"peak" codec:"peak"`
+	Mean    float64 `json:"mean" codec:"mean"`
+	Opened  float64 `json:"opened" codec:"opened"`
+	Min     float64 `json:"min" codec:"min"`
+	Refused float64 `json:"refused" codec:"refused"`
+}
+
+type MessageCount struct {
+	Count int64  `json:"count" codec:"count"`
+	Data  uint64 `json:"data" codec:"data"`
+}
+
+type ConnectionTypes struct {
+	All   ResourceCount `json:"all" codec:"all"`
+	Plain ResourceCount `json:"plain" codec:"plain"`
+	TLS   ResourceCount `json:"tls" codec:"tls"`
+}
+
+type MessageTypes struct {
+	All   MessageCount `json:"all" codec:"all"`
+	Plain MessageCount `json:"plain" codec:"plain"`
+	TLS   MessageCount `json:"tls" codec:"tls"`
+}
+
+type MessageTraffick struct {
+	All      MessageTypes `json:"all" codec:"all"`
+	RealTime MessageTypes `json:"realtime" codec:"realtime"`
+	Rest     MessageTypes `json:"rest" codec:"rest"`
+	Webhook  MessageTypes `json:"webhook" codec:"webhook"`
+}
+
+type RequestCount struct {
+	Failed    float64 `json:"failed" codec:"failed"`
+	Refused   float64 `json:"refused" codec:"refused"`
+	Succeeded float64 `json:"succeeded" codec:"succeeded"`
 }
