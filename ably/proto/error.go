1162ca9f985889ae848cdb3a5501eafc2cfc561b
gernest <geofreyernest@live.com>
2018-10-11 14:22:17 +0300

    fix ProtocolMessage unmasrhsaling

--- ably/proto/error.go
+++ ably/proto/error.go
@@ -10,6 +10,21 @@ type Error struct {
 	Server     string `json:"serverId,omitempty" codec:"serverId,omitempty"`
 }
 
+func (e *Error) FromMap(ctx map[string]interface{}) {
+	if v, ok := ctx["statusCode"]; ok {
+		e.StatusCode = int(coerceInt64(v))
+	}
+	if v, ok := ctx["code"]; ok {
+		e.Code = coerceInt(v)
+	}
+	if v, ok := ctx["message"]; ok {
+		e.Message = v.(string)
+	}
+	if v, ok := ctx["serverId"]; ok {
+		e.Server = v.(string)
+	}
+}
+
 // Error implements the builtin error interface.
 func (e *Error) Error() string {
 	return fmt.Sprintf("%s (status=%d, internal=%d)", e.Message, e.StatusCode, e.Code)
