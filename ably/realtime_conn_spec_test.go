aed7b4bfd3d0b393afddb2ffd45ff17117012dac
gernest <geofreyernest@live.com>
2020-05-11 13:49:02 +0300

    add test for (RTN15b)

--- ably/realtime_conn_spec_test.go
+++ ably/realtime_conn_spec_test.go
@@ -1,6 +1,7 @@
 package ably_test
 
 import (
+	"fmt"
 	"io"
 	"net/url"
 	"testing"
@@ -102,7 +103,8 @@ func TestRealtimeConn_RTN15a_ReconnectOnEOF(t *testing.T) {
 
 type protoConnWithFakeEOF struct {
 	proto.Conn
-	doEOF <-chan struct{}
+	doEOF     <-chan struct{}
+	onMessage func(msg *proto.ProtocolMessage)
 }
 
 func (c protoConnWithFakeEOF) Receive(deadline time.Time) (*proto.ProtocolMessage, error) {
@@ -121,8 +123,147 @@ func (c protoConnWithFakeEOF) Receive(deadline time.Time) (*proto.ProtocolMessag
 
 	select {
 	case r := <-received:
+		if c.onMessage != nil {
+			c.onMessage(r.msg)
+		}
 		return r.msg, r.err
 	case <-c.doEOF:
 		return nil, io.EOF
 	}
+}
+
+func TestRealtimeConn_RTN15b(t *testing.T) {
+	t.Parallel()
+
+	doEOF := make(chan struct{}, 1)
+
+	type meta struct {
+		dial     *url.URL
+		messages []*proto.ProtocolMessage
+	}
+
+	var metaList []*meta
+
+	app, client := ablytest.NewRealtimeClient(&ably.ClientOptions{
+		NoConnect: true,
+		Dial: func(protocol string, u *url.URL) (proto.Conn, error) {
+			m := &meta{dial: u}
+			metaList = append(metaList, m)
+			c, err := ablyutil.DialWebsocket(protocol, u)
+			return protoConnWithFakeEOF{Conn: c, doEOF: doEOF, onMessage: func(msg *proto.ProtocolMessage) {
+				m.messages = append(m.messages, msg)
+			}}, err
+		},
+	})
+	defer safeclose(t, client, app)
+
+	if err := ablytest.Wait(client.Connection.Connect()); err != nil {
+		t.Fatalf("Connect=%s", err)
+	}
+
+	channel := client.Channels.Get("channel")
+
+	if err := ablytest.Wait(channel.Attach()); err != nil {
+		t.Fatal(err)
+	}
+
+	sub, err := channel.Subscribe()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	stateChanges := make(chan ably.State, 16)
+	client.Connection.On(stateChanges)
+
+	doEOF <- struct{}{}
+
+	var state ably.State
+
+	select {
+	case state = <-stateChanges:
+	case <-time.After(50 * time.Millisecond):
+		t.Fatal("didn't transition on EOF")
+	}
+
+	if expected, got := ably.StateConnDisconnected, state; expected != got.State {
+		t.Fatalf("expected transition to %v, got %v", expected, got)
+	}
+
+	// Publish a message to the channel through REST. If connection recovery
+	// succeeds, we should then receive it without reattaching.
+
+	rest, err := ably.NewRestClient(app.Options())
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = rest.Channels.Get("channel", nil).Publish("name", "data")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	select {
+	case state = <-stateChanges:
+	case <-time.After(50 * time.Millisecond):
+		t.Fatal("didn't reconnect")
+	}
+
+	if expected, got := ably.StateConnConnecting, state; expected != got.State {
+		t.Fatalf("expected transition to %v, got %v", expected, got)
+	}
+
+	select {
+	case state = <-stateChanges:
+	case <-time.After(ablytest.Timeout):
+		t.Fatal("didn't transition from CONNECTING")
+	}
+
+	if expected, got := ably.StateConnConnected, state; expected != got.State {
+		t.Fatalf("expected transition to %v, got %v", expected, got)
+	}
+
+	select {
+	case msg := <-sub.MessageChannel():
+		if expected, got := "data", msg.Data; expected != got {
+			t.Fatalf("expected message with data %v, got %v", expected, got)
+		}
+	case <-time.After(ablytest.Timeout):
+		t.Fatal("expected message after connection recovery; got none")
+	}
+
+	if len(metaList) != 2 {
+		t.Fatalf("expected 2 connection dialing got %d", len(metaList))
+	}
+
+	{ //(RTN15b1)
+		u := metaList[1].dial
+		resume := u.Query().Get("resume")
+		connKey := recent(metaList[0].messages, proto.ActionConnected).ConnectionDetails.ConnectionKey
+		if resume == "" {
+			t.Fatal("expected resume query param to be set")
+		}
+		if resume != connKey {
+			t.Errorf("resume: expected %q got %q", connKey, resume)
+		}
+	}
+
+	{ //(RTN15b2)
+		u := metaList[1].dial
+		serial := u.Query().Get("connectionSerial")
+		connSerial := fmt.Sprint(metaList[0].messages[0].ConnectionSerial)
+		if serial == "" {
+			t.Fatal("expected connectionSerial query param to be set")
+		}
+		if serial != connSerial {
+			t.Errorf("connectionSerial: expected %q got %q", connSerial, serial)
+		}
+	}
+}
+
+func recent(msgs []*proto.ProtocolMessage, action proto.Action) *proto.ProtocolMessage {
+	for i := len(msgs) - 1; i >= 0; i-- {
+		if msgs[i].Action == action {
+			return msgs[i]
+		}
+	}
+	return nil
 }
