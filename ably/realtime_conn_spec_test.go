7a72c28af730c26a38bd2bb852a0e600f7f6278f
gernest <geofreyernest@live.com>
2020-07-09 11:18:16 +0300

    add test for RTN23b

--- ably/realtime_conn_spec_test.go
+++ ably/realtime_conn_spec_test.go
@@ -957,3 +957,25 @@ func TestRealtimeConn_RTN15c4(t *testing.T) {
 		t.Fatalf("expected transition to %v, got %v", expected, got)
 	}
 }
+func TestRealtimeConn_RTN23(t *testing.T) {
+	t.Parallel()
+	var query url.Values
+	app, c := ablytest.NewRealtime(&ably.ClientOptions{
+		Dial: func(protocol string, u *url.URL) (proto.Conn, error) {
+			query = u.Query()
+			return ablyutil.DialWebsocket(protocol, u)
+		},
+	})
+	defer safeclose(t, ablytest.FullRealtimeCloser(c), app)
+
+	err := ablytest.ConnWaiter(c, c.Connect, ably.ConnectionEventConnected).Wait()
+	if err != nil {
+		t.Fatal(err)
+	}
+	{ // RTN23b
+		h := query.Get("heartbeats")
+		if h != "true" {
+			t.Errorf("expected heartbeats query param to be true got %q", h)
+		}
+	}
+}
