a8a4b8fc0df08d2e423d48d47107c38770539b04
Toni Cárdenas <toni@tcardenas.me>
2020-10-17 11:23:15 +0200

    TestQueuedEmitter: don't assume Nack will make up an error if not given.

--- ably/state_test.go
+++ ably/state_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 var errNotEmitted = errors.New("not emitted")
+var errNack = NewErrorInfo(123123, errors.New("nack"))
 
 func chans(n int) []chan error {
 	ch := make([]chan error, n)
@@ -28,9 +29,9 @@ func receive(ch ...chan error) []error {
 	return errs
 }
 
-func emit(serial int64, count int, fn func(*pendingEmitter, int64, int, *ErrorInfo)) func(*pendingEmitter) {
+func emit(serial int64, count int, fn func(*pendingEmitter, int64, int, *ErrorInfo), err *ErrorInfo) func(*pendingEmitter) {
 	return func(q *pendingEmitter) {
-		fn(q, serial, count, nil)
+		fn(q, serial, count, err)
 	}
 }
 
@@ -75,34 +76,34 @@ func TestQueuedEmitter(t *testing.T) {
 	}{{ // 5 pending messages, ack first two
 		serial: []int64{1, 2, 3, 4, 5},
 		ack:    []int64{1, 2},
-		emit:   emit(1, 2, (*pendingEmitter).Ack),
+		emit:   emit(1, 2, (*pendingEmitter).Ack, nil),
 	}, { // 5 pending messages, ack for second and third, first should got nack
 		serial: []int64{1, 2, 3, 4, 5},
 		ack:    []int64{2, 3},
 		nack:   []int64{1},
-		emit:   emit(2, 2, (*pendingEmitter).Ack),
+		emit:   emit(2, 2, (*pendingEmitter).Ack, nil),
 	}, { // 5 pending messages, 1 and 2 get nack, rest ack
 		serial: []int64{5, 2, 4, 1, 3},
 		ack:    []int64{3, 4, 5},
 		nack:   []int64{1, 2},
-		emit:   emit(3, 3, (*pendingEmitter).Ack),
+		emit:   emit(3, 3, (*pendingEmitter).Ack, nil),
 	}, { // 10 pending messages, first 5 get nacks, next 4 ack
 		serial: []int64{9, 4, 2, 6, 10, 3, 1, 7, 5, 8},
 		ack:    []int64{6, 7, 8, 9},
 		nack:   []int64{1, 2, 3, 4, 5},
-		emit:   emit(6, 4, (*pendingEmitter).Ack),
+		emit:   emit(6, 4, (*pendingEmitter).Ack, nil),
 	}, { // 5 pending messages, nack first 3
 		serial: []int64{3, 5, 1, 4, 2},
 		nack:   []int64{1, 2, 3},
-		emit:   emit(1, 3, (*pendingEmitter).Nack),
+		emit:   emit(1, 3, (*pendingEmitter).Nack, errNack),
 	}, { // 5 pending messages, nack all
 		serial: []int64{2, 1, 3, 5, 4},
 		nack:   []int64{1, 2, 3, 4, 5},
-		emit:   emit(1, 5, (*pendingEmitter).Nack),
+		emit:   emit(1, 5, (*pendingEmitter).Nack, errNack),
 	}, { // 5 pending messages, nack all
 		serial: []int64{4, 5, 2, 3, 1},
 		nack:   []int64{1, 2, 3, 4, 5},
-		emit:   emit(2, 10, (*pendingEmitter).Nack),
+		emit:   emit(2, 10, (*pendingEmitter).Nack, errNack),
 	}}
 	for _, cas := range cases {
 		testQueuedEmitter(t, cas.serial, cas.ack, cas.nack, cas.emit)
