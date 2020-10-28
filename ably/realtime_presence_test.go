a9b0141fa141a776e5f5eb9694034a18afb9c185
Toni Cárdenas <toni@tcardenas.me>
2020-10-28 08:42:40 +0100

    RealtimePresence: fully convert to 1.2 API.

--- ably/realtime_presence_test.go
+++ ably/realtime_presence_test.go
@@ -45,7 +45,7 @@ func TestRealtimePresence_Sync(t *testing.T) {
 	app, client := ablytest.NewRealtime(nil...)
 	defer safeclose(t, ablytest.FullRealtimeCloser(client), app)
 
-	members, err := client.Channels.Get("persisted:presence_fixtures").Presence.Get(true)
+	members, err := client.Channels.Get("persisted:presence_fixtures").Presence.Get(context.Background())
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -83,7 +83,8 @@ func TestRealtimePresence_Sync250(t *testing.T) {
 	var rg ablytest.ResultGroup
 	var clients = generateClients(250)
 	for _, client := range clients {
-		rg.Add(channel1.Presence.EnterClient(client, ""))
+		client := client
+		rg.GoAdd(func() error { return channel1.Presence.EnterClient(context.Background(), client, "") })
 	}
 	if err := rg.Wait(); err != nil {
 		t.Fatalf("rg.Wait()=%v", err)
@@ -103,7 +104,7 @@ func TestRealtimePresence_Sync250(t *testing.T) {
 	if err = contains(members2, clients...); err != nil {
 		t.Fatalf("members2: %v", err)
 	}
-	members3, err := client3.Channels.Get("sync250").Presence.Get(true)
+	members3, err := client3.Channels.Get("sync250").Presence.Get(context.Background())
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -131,7 +132,7 @@ func TestRealtimePresence_EnsureChannelIsAttached(t *testing.T) {
 	if err := ablytest.ConnWaiter(client, client.Connect, ably.ConnectionEventConnected).Wait(); err != nil {
 		t.Fatal(err)
 	}
-	members, err := channel.Presence.Get(true)
+	members, err := channel.Presence.Get(context.Background())
 	if err != nil {
 		t.Fatal(err)
 	}
