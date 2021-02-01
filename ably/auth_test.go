ably/ably-go

commit 0052ac74a01ed60c7b45a71eae24e4849cf4d513

Fix TestAuth_ClientID.

ably/auth_test.go
@@ -703,17 +703,27 @@ func TestAuth_ClientID(t *testing.T) {
 	tokenErrorDeadline := tokenExpiredAt.Add(5 * time.Second)
 	for err == nil && time.Now().Before(tokenErrorDeadline) {
 		time.Sleep(100 * time.Millisecond)
-		closed := &proto.ProtocolMessage{
-			Action: proto.ActionClosed,
-		}
-		in <- closed
-		if err := ablytest.FullRealtimeCloser(client).Close(); err != nil {
+
+		err = ablytest.Wait(ablytest.ConnWaiter(client, client.Close, ably.ConnectionEventClosing), nil)
+		if err != nil {
 			t.Fatalf("Close()=%v", err)
 		}
 
-		in <- connected
+		err = ablytest.Wait(ablytest.ConnWaiter(client, func() {
+			closed := &proto.ProtocolMessage{
+				Action: proto.ActionClosed,
+			}
+			in <- closed
+		}, ably.ConnectionEventClosed), nil)
+		if err != nil {
+			t.Fatalf("waiting for close: %v", err)
+		}
+
 		proxy.TokenQueue = append(proxy.TokenQueue, tok)
-		err = ablytest.ConnWaiter(client, client.Connect, ably.ConnectionEventConnected).Wait()
+		err = ablytest.Wait(ablytest.ConnWaiter(client, client.Connect,
+			ably.ConnectionEventConnected,
+			ably.ConnectionEventFailed,
+		), nil)
 	}
 	if err = checkError(40012, err); err != nil {
 		t.Fatal(err)

commit 905e37b43f23cdddc40c9f6fa8e1e512c724bb06

Fix regression in TestAuth_ClientID's flakiness.
Fixes a regression introduced in 0052ac7 by which reconnections never
successfully connected, so, if at the first try the token wasn't
expired, the test got stuck until the timeout.

Also, document a bit better what's going on.

ably/auth_test.go
@@ -680,12 +680,20 @@ func TestAuth_ClientID(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	// After the current token expires, reconnecting should request a new token
+	// from authURL. Make it return the token with the mismatched client ID, and
+	// expect a transition to FAILED.
+
 	time.Sleep(time.Duration(params.TTL) * time.Millisecond)
 	tokenExpiredAt := time.Now()
 
 	// Allow some extra time for the server to reject our token.
-	err = nil
 	tokenErrorDeadline := tokenExpiredAt.Add(5 * time.Second)
+
+	// Close and reconnect with the expired token, until we get an error.
+	// Expiration deadlines are inexact, so this may take a few tries until the
+	// token is effectively expired.
+	err = nil
 	for err == nil && time.Now().Before(tokenErrorDeadline) {
 		time.Sleep(100 * time.Millisecond)
 
@@ -704,6 +712,7 @@ func TestAuth_ClientID(t *testing.T) {
 			t.Fatalf("waiting for close: %v", err)
 		}
 
+		in <- connected
 		proxy.TokenQueue = append(proxy.TokenQueue, tok)
 		err = ablytest.Wait(ablytest.ConnWaiter(client, client.Connect,
 			ably.ConnectionEventConnected,