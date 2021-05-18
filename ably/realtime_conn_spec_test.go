32a8542bdd30047c43a35424be4f01a19207790a
sacOO7 <sachinshinde7676@gmail.com>
2021-05-18 07:33:22 +0530

    Refactored tests, added way to pass options externally to setup

--- ably/realtime_conn_spec_test.go
+++ ably/realtime_conn_spec_test.go
@@ -21,70 +21,82 @@ import (
 
 func Test_RTN2_WebsocketQueryParams(t *testing.T) {
 	t.Parallel()
+	setup := func(options ...ably.ClientOption) (requestParams url.Values) {
+		in := make(chan *proto.ProtocolMessage, 1)
+		out := make(chan *proto.ProtocolMessage, 16)
+		var urls []url.URL
+		defaultOptions := []ably.ClientOption{
+			ably.WithToken("fake:token"),
+			ably.WithAutoConnect(false),
+			ably.WithDial(func(proto string, u *url.URL, timeout time.Duration) (proto.Conn, error) {
+				urls = append(urls, *u)
+				return ablytest.MessagePipe(in, out)(proto, u, timeout)
+			}),
+		}
+		options = append(defaultOptions, options...)
+		c, _ := ably.NewRealtime(options...)
+		in <- &proto.ProtocolMessage{
+			Action:            proto.ActionConnected,
+			ConnectionID:      "connection-id",
+			ConnectionDetails: &proto.ConnectionDetails{},
+		}
 
-	in := make(chan *proto.ProtocolMessage, 1)
-	out := make(chan *proto.ProtocolMessage, 16)
-	token := "fake:token"
-	useBinaryProtocol := false
-	var urls []url.URL
-	c, _ := ably.NewRealtime(
-		ably.WithAutoConnect(false),
-		ably.WithToken(token),
-		ably.WithUseBinaryProtocol(useBinaryProtocol),
-		ably.WithDial(func(proto string, u *url.URL, timeout time.Duration) (proto.Conn, error) {
-			urls = append(urls, *u)
-			return ablytest.MessagePipe(in, out)(proto, u, timeout)
-		}))
-
-	in <- &proto.ProtocolMessage{
-		Action:            proto.ActionConnected,
-		ConnectionID:      "connection-id",
-		ConnectionDetails: &proto.ConnectionDetails{},
+		err := ablytest.Wait(ablytest.ConnWaiter(c, c.Connect, ably.ConnectionEventConnected), nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		requestParams = urls[0].Query()
+		return
 	}
 
-	err := ablytest.Wait(ablytest.ConnWaiter(c, c.Connect, ably.ConnectionEventConnected), nil)
-	if err != nil {
-		t.Fatal(err)
+	getProtocol := func(useBinaryProtocol bool) string {
+		if useBinaryProtocol {
+			return "msgpack"
+		}
+		return "json"
 	}
-
-	requestParams := urls[0].Query()
 
 	t.Run("RTN2a: format should be msgPack or json", func(t *testing.T) {
 		t.Parallel()
-		expectedProtocol := "json"
-		if useBinaryProtocol {
-			expectedProtocol = "msgpack"
-		}
+		useBinaryProtocol := false
+		requestParams := setup(ably.WithUseBinaryProtocol(useBinaryProtocol))
+		expectedProtocol := getProtocol(useBinaryProtocol)
 		protocol := requestParams["format"]
 		assertDeepEquals(t, []string{expectedProtocol}, protocol)
 	})
 
 	t.Run("RTN2b: echo should be true by default", func(t *testing.T) {
 		t.Parallel()
+		requestParams := setup()
 		echo := requestParams["echo"]
 		assertDeepEquals(t, []string{"true"}, echo)
 	})
 
 	t.Run("RTN2d: clientId contains provided clientId", func(t *testing.T) {
 		t.Parallel()
+		requestParams := setup()
 		clientId := requestParams["clientId"]
 		assertNil(t, clientId)
 	})
 
 	t.Run("RTN2e: depending on the auth scheme, accessToken contains token string or key contains api key", func(t *testing.T) {
 		t.Parallel()
+		token := "fake:clientToken"
+		requestParams := setup(ably.WithToken(token))
 		actualToken := requestParams["access_token"]
 		assertDeepEquals(t, []string{token}, actualToken)
 	})
 
 	t.Run("RTN2f: api version v should be the API version", func(t *testing.T) {
 		t.Parallel()
+		requestParams := setup()
 		libVersion := requestParams["v"]
 		assertDeepEquals(t, []string{"1.2"}, libVersion)
 	})
 
 	t.Run("RTN2g: library and version should be included as the value of lib param", func(t *testing.T) {
 		t.Parallel()
+		requestParams := setup()
 		lib := requestParams["lib"]
 		assertDeepEquals(t, []string{"go-1.2.0-apipreview.4"}, lib)
 	})
