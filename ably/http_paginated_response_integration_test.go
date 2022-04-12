c02b449e73f69dc98514d02c763ea3a0165f74c4
Morgan Adamiec <morgan.adamiec@ably.com>
2022-04-12 13:53:57 +0100

    Add test for paginated fallback

--- ably/http_paginated_response_integration_test.go
+++ ably/http_paginated_response_integration_test.go
@@ -16,6 +16,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestHTTPPaginatedFallback(t *testing.T) {
+	app, err := ablytest.NewSandbox(nil)
+	assert.NoError(t, err)
+	defer app.Close()
+	opts := app.Options(ably.WithUseBinaryProtocol(false), ably.WithRESTHost("ably.invalid"), ably.WithFallbackHostsUseDefault(true))
+	client, err := ably.NewREST(opts...)
+	assert.NoError(t, err)
+	t.Run("request_time", func(t *testing.T) {
+		_, err := client.Request("get", "/time").Pages(context.Background())
+		assert.Error(t, err)
+	})
+}
+
 func TestHTTPPaginatedResponse(t *testing.T) {
 	app, err := ablytest.NewSandbox(nil)
 	assert.NoError(t, err)
