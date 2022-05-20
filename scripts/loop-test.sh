b2ca2478e42c56176a46cf1b95dabb603c3a9d9a
Rosie Hamilton <enchantrix@gmail.com>
2022-05-20 12:18:44 +0100

    run ci

--- scripts/loop-test.sh
+++ scripts/loop-test.sh
@@ -3,9 +3,9 @@
 # A script to run a test in a loop to see if it is flakey
 
 cd ably
-for ((i=1;i<=100;i++)); 
+for ((i=1;i<=10;i++)); 
 do 
    echo $i
-   go test -v -run TestX
+   go test -v -run TestRealtimeConn_RTN15b
    go clean -testcache
 done
