cc99bfea6edc280e674f1dfc126653cd2e74bedf
Rosie Hamilton <enchantrix@gmail.com>
2022-06-06 12:10:12 +0100

    run TestRealtimePresence_Sync250 in a loop

---
+++ scripts/loop-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# A script to run a test in a loop to see if it is flakey
+
+cd ably
+for ((i=1;i<=100;i++)); 
+do 
+   echo $i
+   go test -v -run TestRealtimePresence_Sync250
+   go clean -testcache
+done
