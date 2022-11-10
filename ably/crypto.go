4748fa56c20cbf749f71e8c316b76b11143cd713
sacOO7 <sachinshinde7676@gmail.com>
2022-11-11 00:40:51 +0530

    Added documentation to proto_crypto and crypto

--- ably/crypto.go
+++ ably/crypto.go
@@ -7,13 +7,28 @@ import (
 	"io"
 )
 
+// **CANONICAL**
+// Contains the properties required to configure the encryption of [Message]{@link Message} payloads.
 var Crypto struct {
+
+	// **LEGACY**
 	// GenerateRandomKey returns a random key. keyLength is optional; if
 	// non-zero, it should be in bits.
+	// **CANONICAL**
+	// Generates a random key to be used in the encryption of the channel. If the language cryptographic randomness primitives are blocking or async, a callback is used. The callback returns a generated binary key.
+	// keyLength - The length of the key, in bits, to be generated. If not specified, this is equal to the default keyLength of the default algorithm: for AES this is 256 bits.
+	// Returns - The key as a binary, for example, a byte array.
+	// RSE2
 	GenerateRandomKey func(keyLength int) ([]byte, error)
 
+	// **LEGACY**
 	// GetDefaultParams returns the provided CipherParams with missing fields
 	// set to default values. The Key field must be provided.
+	// **CANONICAL**
+	// Returns a [CipherParams]{@link CipherParams} object, using the default values for any fields not supplied by the [CipherParamOptions]{@link CipherParamOptions} object.
+	// CipherParamOptions - A [CipherParamOptions]{@link CipherParamOptions} object.
+	// Returns - A [CipherParams]{@link CipherParams} object, using the default values for any fields not supplied.
+	// RSE1
 	GetDefaultParams func(CipherParams) CipherParams
 }
 
--- ably/proto_crypto.go
+++ ably/proto_crypto.go
@@ -62,20 +62,37 @@ const (
 // CipherParams  provides parameters for configuring encryption  for channels.
 //
 //Spec item (TZ1)
+// **CANONICAL**
+// Sets the properties to configure encryption for a [RestChannel]{@link RestChannel} or [RealtimeChannel]{@link RealtimeChannel} object.
 type CipherParams struct {
-	Algorithm CipherAlgorithm // Spec item (TZ2a)
+	// **CANONICAL**
+	// The algorithm to use for encryption. Only AES is supported and is the default value.
+	// TZ2a
+	Algorithm CipherAlgorithm
+	// **LEGACY**
 	// The length of the private key in bits
-	KeyLength int // Spec item (TZ2b)
+	// **CANONICAL**
+	// The length of the key in bits; for example 128 or 256.
+	// TZ2b
+	KeyLength int
+	// **LEGACY**
 	// This is the private key used to  encrypt/decrypt payloads.
-	Key []byte // Spec item (TZ2d)
+	// **CANONICAL**
+	// The private key used to encrypt and decrypt payloads.
+	// TZ2d
+	Key []byte
+	// **LEGACY**
 	// The cipher mode to be used for encryption default is CBC.
 	//
 	// Spec item (TZ2c)
+	// **CANONICAL**
+	// The cipher mode. Only CBC is supported and is the default value.
 	Mode CipherMode
 
+	// **LEGACY**
 	// iv is the initialization vector. Used only for comparing resulting
 	// ciphertext with test fixtures; production code should always use a random
-	// unique IV per encrypted message.
+	// unique IV per encrypted message.CipherParamOptions
 	iv []byte
 }
 
