492554187a6696f6451e1e8f56f2e9862d43f887
sacOO7 <sachinshinde7676@gmail.com>
2022-11-20 00:54:57 +0530

    Refactored canonical doc for channel options to lang specific doc

--- ably/proto_channel_options.go
+++ ably/proto_channel_options.go
@@ -6,25 +6,13 @@ type channelParams map[string]string
 type ChannelMode int64
 
 const (
-	// **LEGACY**
-	// Presence mode. Allows the attached channel to enter Presence.
-	// **CANONICAL**
-	// The client can enter the presence set.
+	// ChannelModePresence allows the attached channel to enter Presence.
 	ChannelModePresence ChannelMode = iota + 1
-	// **LEGACY**
-	// Publish mode. Allows the messages to be published to the attached channel.
-	// **CANONICAL**
-	// The client can publish messages..
+	// ChannelModePublish allows for messages to be published on the attached channel.
 	ChannelModePublish
-	// **LEGACY**
-	// Subscribe mode. Allows the attached channel to subscribe to messages.
-	// **CANONICAL**
-	// The client can subscribe to messages.
+	// ChannelModeSubscribe allows the attached channel to subscribe to messages.
 	ChannelModeSubscribe
-	// **LEGACY**
-	// PresenceSubscribe. Allows the attached channel to subscribe to Presence updates.
-	// **CANONICAL**
-	// The client can receive presence messages.
+	// ChannelModePresenceSubscribe allows the attached channel to subscribe to Presence updates.
 	ChannelModePresenceSubscribe
 )
 
@@ -60,10 +48,9 @@ func channelModeFromFlag(flags protoFlag) []ChannelMode {
 	return modes
 }
 
-// **LEGACY**
-// protoChannelOptions defines options provided for creating a new channel.
-// **CANONICAL**
-// Passes additional properties to a [RestChannel]{@link RestChannel} or [RealtimeChannel]{@link RealtimeChannel} object, such as encryption, [ChannelMode]{@link ChannelMode} and channel parameters.
+// protoChannelOptions defines additional properties to a [ably.RESTChannel] or [ably.RealtimeChannel] object,
+// such as encryption, [ably.ChannelMode] and channel parameters.
+// It defines options provided for creating a new channel.
 type protoChannelOptions struct {
 	Cipher CipherParams
 	cipher channelCipher
