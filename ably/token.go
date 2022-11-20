4015fb280efe48dd7d7ad2b67dda18bfb94c6127
sacOO7 <sachinshinde7676@gmail.com>
2022-11-21 03:24:18 +0530

    Refactored canonical doc for token to lang specific doc

--- ably/token.go
+++ ably/token.go
@@ -12,38 +12,35 @@ import (
 
 // TokenParams contains token params to be sent to ably to get auth token
 type TokenParams struct {
-	// **LEGACY**
-	// TTL is a requested time to live for the token. If the token request
+	// TTL is a requested time to live for the token in milliseconds. If the token request
 	// is successful, the TTL of the returned token will be less than or equal
 	// to this value depending on application settings and the attributes
 	// of the issuing key.
-	// **CANONICAL**
-	// Default 60 min
-	// Requested time to live for the token in milliseconds. The default is 60 minutes.
-	// RSA9e, TK2a
+	// The default is 60 minutes (RSA9e, TK2a).
 	TTL int64 `json:"ttl,omitempty" codec:"ttl,omitempty"`
 
-	// **LEGACY**
-	// Capability represents encoded access rights of the token.
-	// **CANONICAL**
-	// default '{"*":["*"]}'
-	// The capabilities associated with this Ably Token. The capabilities value is a JSON-encoded representation of the resource paths and associated operations. Read more about capabilities in the capabilities docs.
-	// RSA9f, TK2b
+	// Capability represents encoded channel access rights associated with this Ably Token.
+	// The capabilities value is a JSON-encoded representation of the resource paths and associated operations.
+	// Read more about capabilities in the [capabilities docs].
+	// default '{"*":["*"]}' (RSA9f, TK2b)
+	//
+	// [capabilities docs]: https://ably.com/docs/core-features/authentication/#capabilities-explained
 	Capability string `json:"capability,omitempty" codec:"capability,omitempty"`
 
-	// **LEGACY**
-	// ClientID represents a client, whom the token is generated for.
-	// **CANONICAL**
-	// A client ID, used for identifying this client when publishing messages or for presence purposes. The clientId can be any non-empty string, except it cannot contain a *. This option is primarily intended to be used in situations where the library is instantiated with a key. Note that a clientId may also be implicit in a token used to instantiate the library. An error is raised if a clientId specified here conflicts with the clientId implicit in the token. Find out more about identified clients.
-	// TK2c
+	// ClientID is used for identifying this client when publishing messages or for presence purposes.
+	// The clientId can be any non-empty string, except it cannot contain a *. This option is primarily intended
+	// to be used in situations where the library is instantiated with a key.
+	// Note that a clientId may also be implicit in a token used to instantiate the library.
+	// An error is raised if a clientId specified here conflicts with the clientId implicit in the token.
+	// Find out more about [identified clients] (TK2c).
+	//
+	// [identified clients]: https://ably.com/docs/core-features/authentication#identified-clients
 	ClientID string `json:"clientId,omitempty" codec:"clientId,omitempty"`
 
-	// **LEGACY**
-	// Timestamp of the token request. It's used, in conjunction with the nonce,
-	// are used to prevent token requests from being replayed.
-	// **CANONICAL**
-	// The timestamp of this request as milliseconds since the Unix epoch. Timestamps, in conjunction with the nonce, are used to prevent requests from being replayed. timestamp is a "one-time" value, and is valid in a request, but is not validly a member of any default token params such as ClientOptions.defaultTokenParams.
-	// RSA9d, Tk2d
+	// Timestamp of the token request as milliseconds since the Unix epoch.
+	// Timestamps, in conjunction with the nonce, are used to prevent requests from being replayed.
+	// timestamp is a "one-time" value, and is valid in a request, but is not validly a member of
+	// any default token params such as ClientOptions.defaultTokenParams (RSA9d, Tk2d).
 	Timestamp int64 `json:"timestamp,omitempty" codec:"timestamp,omitempty"`
 }
 
@@ -73,20 +70,15 @@ func (params *TokenParams) Query() url.Values {
 type TokenRequest struct {
 	TokenParams `codec:",inline"`
 
-	// **CANONICAL**
-	// The name of the key against which this request is made. The key name is public, whereas the key secret is private.
-	// TE2
+	// KeyName is the name of the key against which this request is made.
+	// The key name is public, whereas the key secret is private (TE2).
 	KeyName string `json:"keyName,omitempty" codec:"keyName,omitempty"`
-	// **LEGACY**
-	// should be at least 16 characters long
-	// **CANONICAL**
-	// A cryptographically secure random string of at least 16 characters, used to ensure the TokenRequest cannot be reused.
-	// TE2
+
+	// Nonce is a cryptographically secure random string of at least 16 characters,
+	// used to ensure the TokenRequest cannot be reused (TE2).
 	Nonce   string `json:"nonce,omitempty" codec:"nonce,omitempty"`
-	// **LEGACY**
-	// message authentication code for the request
-	// **CANONICAL**
-	// The Message Authentication Code for this request.
+
+	// MAC is the Message Authentication Code for this request.
 	MAC     string `json:"mac,omitempty" codec:"mac,omitempty"`
 }
 
@@ -104,46 +96,32 @@ func (req *TokenRequest) sign(secret []byte) {
 	req.MAC = base64.StdEncoding.EncodeToString(mac.Sum(nil))
 }
 
-// **LEGACY**
-// TokenDetails
-// **CANONICAL**
-// Contains an Ably Token and its associated metadata.
+// TokenDetails contains an Ably Token and its associated metadata.
 type TokenDetails struct {
-	// **LEGACY**
-	// Token
-	// **CANONICAL**
-	// The Ably Token itself. A typical Ably Token string appears with the form xVLyHw.A-pwh7wicf3afTfgiw4k2Ku33kcnSA7z6y8FjuYpe3QaNRTEo4.
-	// TD2
+
+	// Token is the ably Token itself (TD2).
+	// A typical Ably Token string appears with the form xVLyHw.A-pwh7wicf3afTfgiw4k2Ku33kcnSA7z6y8FjuYpe3QaNRTEo4.
 	Token string `json:"token,omitempty" codec:"token,omitempty"`
 
 	// KeyName is a string part of ABLY_KEY before :
 	KeyName string `json:"keyName,omitempty" codec:"keyName,omitempty"`
 
-	// **LEGACY**
-	// Expires
-	// **CANONICAL**
-	// The timestamp at which this token expires as milliseconds since the Unix epoch.
-	// TD3
+	// Expires is the timestamp at which this token expires as milliseconds since the Unix epoch (TD3).
 	Expires int64 `json:"expires,omitempty" codec:"expires,omitempty"`
 
-	// **LEGACY**
-	// ClientID
-	// **CANONICAL**
-	// The client ID, if any, bound to this Ably Token. If a client ID is included, then the Ably Token authenticates its bearer as that client ID, and the Ably Token may only be used to perform operations on behalf of that client ID. The client is then considered to be an identified client.
-	// TD6
+	// ClientID, if any, bound to this Ably Token. If a client ID is included, then the Ably Token authenticates
+	// its bearer as that client ID, and the Ably Token may only be used to perform operations on behalf
+	// of that client ID. The client is then considered to be an identified client (TD6).
 	ClientID string `json:"clientId,omitempty" codec:"clientId,omitempty"`
 
-	// **LEGACY**
-	// Issued
-	// **CANONICAL**
-	// The timestamp at which this token was issued as milliseconds since the Unix epoch.
+	// Issued is the timestamp at which this token was issued as milliseconds since the Unix epoch.
 	Issued int64 `json:"issued,omitempty" codec:"issued,omitempty"`
 
-	// **LEGACY**
-	// Capability
-	// **CANONICAL**
-	// The capabilities associated with this Ably Token. The capabilities value is a JSON-encoded representation of the resource paths and associated operations. Read more about capabilities in the capabilities docs.
-	// TD5
+	// Capability is the capabilities associated with this Ably Token.
+	// The capabilities value is a JSON-encoded representation of the resource paths and associated operations.
+	// Read more about capabilities in the [capabilities docs] (TD5).
+	//
+	// [capabilities docs]: https://ably.com/docs/core-features/authentication/#capabilities-explained
 	Capability string `json:"capability,omitempty" codec:"capability,omitempty"`
 }
 
