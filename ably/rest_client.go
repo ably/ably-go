69f6c42716ffcd3cf5f0036dc4cdb3735370ba69
Owen Pearson <owen.pearson@ably.com>
2022-12-12 12:09:48 +0000

    feat: retry on cloudfront http errors

--- ably/rest_client.go
+++ ably/rest_client.go
@@ -685,7 +685,7 @@ func (c *REST) doWithHandle(ctx context.Context, r *request, handle func(*http.R
 	if err != nil {
 		c.log.Error("RestClient: error handling response: ", err)
 		if e, ok := err.(*ErrorInfo); ok {
-			if canFallBack(e.StatusCode) {
+			if canFallBack(e.StatusCode, resp.Header) {
 				fallbacks, _ := c.opts.getFallbackHosts()
 				c.log.Infof("RestClient: trying to fallback with hosts=%v", fallbacks)
 				if len(fallbacks) > 0 {
@@ -735,7 +735,7 @@ func (c *REST) doWithHandle(ctx context.Context, r *request, handle func(*http.R
 								return nil, err
 							}
 							if ev, ok := err.(*ErrorInfo); ok {
-								if canFallBack(ev.StatusCode) {
+								if canFallBack(ev.StatusCode, resp.Header) {
 									iteration++
 									continue
 								}
@@ -764,9 +764,10 @@ func (c *REST) doWithHandle(ctx context.Context, r *request, handle func(*http.R
 	return resp, nil
 }
 
-func canFallBack(code int) bool {
-	return http.StatusInternalServerError <= code &&
-		code <= http.StatusGatewayTimeout
+func canFallBack(code int, header http.Header) bool {
+  return http.StatusInternalServerError <= code &&
+  code <= http.StatusGatewayTimeout ||
+  (header.Get("Server") == "Cloudfront" && http.StatusBadRequest <= code) // RSC15l4
 }
 
 // newHTTPRequest creates a new http.Request that can be sent to ably endpoints.
