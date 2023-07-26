b76504885a169c16b0e3534e0de2f902edc8b984
sacOO7 <sachinshinde7676@gmail.com>
2023-07-27 00:52:49 +0530

    Added implementation for encode method in RecoveryContext struct

--- ably/recovery_context.go
+++ ably/recovery_context.go
@@ -1,12 +1,15 @@
 package ably
 
-// RecoveryKeyContext contains the properties required to recover existing connection.
+import "encoding/json"
+
+// recoveryKeyContext contains the properties required to recover existing connection.
 type recoveryKeyContext struct {
 	ConnectionKey  string            `json:"connectionKey,omitempty" codec:"connectionKey,omitempty"`
 	MsgSerial      int64             `json:"msgSerial,omitempty" codec:"msgSerial,omitempty"`
 	ChannelSerials map[string]string `json:"channelSerials,omitempty" codec:"channelSerials,omitempty"`
 }
 
-func (r *recoveryKeyContext) Encode() string {
-	return ""
+func (r *recoveryKeyContext) Encode() (string, error) {
+	serializedRecoveryKey, err := json.Marshal(r)
+	return string(serializedRecoveryKey), err
 }
