736dfe1776d01408559f65862c247fd4d0b7998f
sacOO7 <sachinshinde7676@gmail.com>
2023-07-27 01:18:26 +0530

    Added decode method to recoveryContext

--- ably/recovery_context.go
+++ ably/recovery_context.go
@@ -4,12 +4,18 @@ import "encoding/json"
 
 // recoveryKeyContext contains the properties required to recover existing connection.
 type recoveryKeyContext struct {
-	ConnectionKey  string            `json:"connectionKey,omitempty" codec:"connectionKey,omitempty"`
-	MsgSerial      int64             `json:"msgSerial,omitempty" codec:"msgSerial,omitempty"`
-	ChannelSerials map[string]string `json:"channelSerials,omitempty" codec:"channelSerials,omitempty"`
+	ConnectionKey  string            `json:"connectionKey" codec:"connectionKey"`
+	MsgSerial      int64             `json:"msgSerial" codec:"msgSerial"`
+	ChannelSerials map[string]string `json:"channelSerials" codec:"channelSerials"`
 }
 
-func (r *recoveryKeyContext) Encode() (string, error) {
-	serializedRecoveryKey, err := json.Marshal(r)
-	return string(serializedRecoveryKey), err
+func (r *recoveryKeyContext) Encode() (serializedRecoveryKey string, err error) {
+	result, err := json.Marshal(r)
+	serializedRecoveryKey = string(result)
+	return
+}
+
+func Decode(recoveryKey string) (rCtx *recoveryKeyContext, err error) {
+	err = json.Unmarshal([]byte(recoveryKey), &rCtx)
+	return
 }
