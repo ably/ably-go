6d8e8869afca4ff84a07d0927641f3c340d3a873
sacOO7 <sachinshinde7676@gmail.com>
2023-07-26 23:58:56 +0530

    Added json serialization annotations to recoveryContext

--- ably/recovery_context.go
+++ ably/recovery_context.go
@@ -2,9 +2,9 @@ package ably
 
 // RecoveryKeyContext contains the properties required to recover existing connection.
 type recoveryKeyContext struct {
-	ConnectionKey  string
-	MsgSerial      int64
-	ChannelSerials map[string]string
+	ConnectionKey  string            `json:"connectionKey,omitempty" codec:"connectionKey,omitempty"`
+	MsgSerial      int64             `json:"msgSerial,omitempty" codec:"msgSerial,omitempty"`
+	ChannelSerials map[string]string `json:"channelSerials,omitempty" codec:"channelSerials,omitempty"`
 }
 
 func (r *recoveryKeyContext) Encode() string {
