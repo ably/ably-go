bcc04206aa3748b1d376c280710d4f1d5b8b3325
sacOO7 <sachinshinde7676@gmail.com>
2023-07-27 23:29:54 +0530

    Added testcases forn recovery context

---
+++ ably/recovery_context_test.go
@@ -0,0 +1,20 @@
+//go:build !unit
+// +build !unit
+
+package ably_test
+
+import (
+	"testing"
+)
+
+func Test_ShouldEncodeRecoveryKeyContextObject(t *testing.T) {
+
+}
+
+func Test_ShouldDecodeRecoveryKeyToRecoveryKeyContextObject(t *testing.T) {
+
+}
+
+func Test_ShouldReturnNullRecoveryContextWhileDecodingFaultyRecoveryKey(t *testing.T) {
+
+}
