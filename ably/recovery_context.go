f52c9a036a8b1bf90f0a68b9b65a3555f4c3c777
sacOO7 <sachinshinde7676@gmail.com>
2023-07-27 23:59:01 +0530

    Added a test to serialize recoveryKey and check the same

--- ably/recovery_context.go
+++ ably/recovery_context.go
@@ -2,20 +2,20 @@ package ably
 
 import "encoding/json"
 
-// recoveryKeyContext contains the properties required to recover existing connection.
-type recoveryKeyContext struct {
+// RecoveryKeyContext contains the properties required to recover existing connection.
+type RecoveryKeyContext struct {
 	ConnectionKey  string            `json:"connectionKey" codec:"connectionKey"`
 	MsgSerial      int64             `json:"msgSerial" codec:"msgSerial"`
 	ChannelSerials map[string]string `json:"channelSerials" codec:"channelSerials"`
 }
 
-func (r *recoveryKeyContext) Encode() (serializedRecoveryKey string, err error) {
+func (r *RecoveryKeyContext) Encode() (serializedRecoveryKey string, err error) {
 	result, err := json.Marshal(r)
 	serializedRecoveryKey = string(result)
 	return
 }
 
-func Decode(recoveryKey string) (rCtx *recoveryKeyContext, err error) {
+func Decode(recoveryKey string) (rCtx *RecoveryKeyContext, err error) {
 	err = json.Unmarshal([]byte(recoveryKey), &rCtx)
 	return
 }
--- ably/recovery_context_test.go
+++ ably/recovery_context_test.go
@@ -5,10 +5,25 @@ package ably_test
 
 import (
 	"testing"
+
+	"github.com/ably/ably-go/ably"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_ShouldEncodeRecoveryKeyContextObject(t *testing.T) {
-
+	var expectedRecoveryKey = "{\"connectionKey\":\"uniqueKey\",\"msgSerial\":1,\"channelSerials\":{\"channel1\":\"1\",\"channel2\":\"2\",\"channel3\":\"3\"}}"
+	var recoveryKey = &ably.RecoveryKeyContext{
+		ConnectionKey: "uniqueKey",
+		MsgSerial:     1,
+		ChannelSerials: map[string]string{
+			"channel1": "1",
+			"channel2": "2",
+			"channel3": "3",
+		},
+	}
+	key, err := recoveryKey.Encode()
+	assert.Nil(t, err)
+	assert.Equal(t, expectedRecoveryKey, key)
 }
 
 func Test_ShouldDecodeRecoveryKeyToRecoveryKeyContextObject(t *testing.T) {
