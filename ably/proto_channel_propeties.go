5781fc7ceee6aa42462d5a20498dbea7d4e58646
sacOO7 <sachinshinde7676@gmail.com>
2023-07-28 22:56:25 +0530

    Created a separate structure type for channelSerial and AttachSerial

---
+++ ably/proto_channel_propeties.go
@@ -0,0 +1,9 @@
+package ably
+
+type ChannelProperties struct {
+	// AttachSerial contains the channelSerial from latest ATTACHED ProtocolMessage received on the channel, see CP2a, RTL15a
+	AttachSerial string
+
+	// ChannelSerial contains the channelSerial from latest ProtocolMessage of action type Message/PresenceMessage received on the channel, see CP2b, RTL15b.
+	ChannelSerial string
+}
