154342c14b6b954bd2ee1b98549d2c9d5a6ca3c9
sacOO7 <sachinshinde7676@gmail.com>
2023-07-31 18:19:55 +0530

    Added test util to generate random string of given size

--- ablytest/ablytest.go
+++ ablytest/ablytest.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"os"
 	"reflect"
@@ -208,4 +209,16 @@ func TimeFuncs(afterCalls chan<- AfterCall) (
 	}
 
 	return now, after
+}
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func GenerateRandomString(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
 }
