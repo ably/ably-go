cefbf8d8dc0fb2b40ba9352708641fb2e425f1b3
sacOO7 <sachinshinde7676@gmail.com>
2023-08-30 17:18:16 +0530

    Removed unnecessary implementation for queueFlush, sending all messages from queue when channelState is attached

--- ably/realtime_presence.go
+++ ably/realtime_presence.go
@@ -88,13 +88,6 @@ func (pres *RealtimePresence) maybeEnqueue(msg *protocolMessage, onAck func(err 
 	return true
 }
 
-func (pres *RealtimePresence) sendLocalPresenceQueueMessages() {
-	if len(pres.queue.queue) == 0 {
-		return
-	}
-	protoPresenceMsg := &protocolMessage{Presence: make([]*PresenceMessage, len(pres.queue.queue))}
-}
-
 func (pres *RealtimePresence) send(msg *PresenceMessage) (result, error) {
 	// RTP16c
 	if err := pres.verifyChanState(); err != nil {
@@ -176,7 +169,7 @@ func (pres *RealtimePresence) onAttach(msg *protocolMessage, isNewAttach bool) {
 			pres.syncMtx.Unlock()
 		}
 	}
-	pres.sendLocalPresenceQueueMessages()
+	pres.queue.Flush()
 }
 
 // SyncComplete gives true if the initial SYNC operation has completed for the members present on the channel.
