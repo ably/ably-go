1e924909f692a41376a6b4992cd9d4d22bfd1b0a
sacOO7 <sachinshinde7676@gmail.com>
2023-10-25 22:50:28 +0530

    Refactored mock channel state for presence, removed unnecessary mock_test

--- ably/realtime_presence_internal_test.go
+++ ably/realtime_presence_internal_test.go
@@ -4,12 +4,38 @@
 package ably
 
 import (
+	"bytes"
 	"context"
 	"errors"
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+var (
+	buffer     bytes.Buffer
+	mocklogger = log.New(&buffer, "logger: ", log.Lshortfile)
+)
+
+// mockChannelWithState is a test helper that returns a mock channel in a specified state
+func mockChannelWithState(channelState *ChannelState, connectionState *ConnectionState) *RealtimeChannel {
+	mockChannel := RealtimeChannel{
+		client: &Realtime{
+			rest: &REST{
+				log: logger{l: &stdLogger{mocklogger}},
+			},
+			Connection: &Connection{},
+		},
+	}
+	if channelState != nil {
+		mockChannel.state = *channelState
+	}
+	if connectionState != nil {
+		mockChannel.client.Connection.state = *connectionState
+	}
+	return &mockChannel
+}
 
 func TestVerifyChanState_RTP16(t *testing.T) {
 	tests := map[string]struct {
