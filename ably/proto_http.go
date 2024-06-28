498be483d69540ad4ef041422eac9e19f293704e
sachin shinde <sachinshinde7676@gmail.com>
2024-06-28 15:20:45 +0000

    bumped up version to 1.2.18

--- ably/proto_http.go
+++ ably/proto_http.go
@@ -11,7 +11,7 @@ const (
 	ablyProtocolVersionHeader = "X-Ably-Version"
 	ablyErrorCodeHeader       = "X-Ably-Errorcode"
 	ablyErrorMessageHeader    = "X-Ably-Errormessage"
-	clientLibraryVersion      = "1.2.17"
+	clientLibraryVersion      = "1.2.18"
 	clientRuntimeName         = "go"
 	ablyProtocolVersion       = "2" // CSV2
 	ablyClientIDHeader        = "X-Ably-ClientId"
